158ef512793bb805a7fe8cc3d5e61466dcc96753
aqeel123gi0ta <a.aljutail@tahdir.sa>
2025-09-28 09:21:31 +0300

    Update flowter_core.dart

--- flowter_core/lib/flowter_core.dart
+++ flowter_core/lib/flowter_core.dart
@@ -1,8 +1,8 @@
-library aq_frame;
+// library aq_frame;
 
-export 'extensions/extensions.dart';
-export 'widgets/widgets.dart';
-export 'enums/enums.dart';
-export 'functions/functions.dart';
-export 'classes/classes.dart';
-export 'template/template.dart';
+// export 'extensions/extensions.dart';
+// export 'widgets/widgets.dart';
+// export 'enums/enums.dart';
+// export 'functions/functions.dart';
+// export 'classes/classes.dart';
+// export 'template/template.dart';
