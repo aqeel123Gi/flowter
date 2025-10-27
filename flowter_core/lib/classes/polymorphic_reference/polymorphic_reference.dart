a1595c71d2c9163922ce4a0611baff77404dcff3
aqeel123gi0ta <a.aljutail@tahdir.sa>
2025-10-27 14:17:59 +0300

    Update polymorphic_reference.dart

--- flowter_core/lib/classes/polymorphic_reference/polymorphic_reference.dart
+++ flowter_core/lib/classes/polymorphic_reference/polymorphic_reference.dart
@@ -1,18 +1,19 @@
-class PolymorphicReference<A, B> {
-  final A id;
-  final B type;
+class PolymorphicReference<T,ID> {
+  
+  final T type;
+  final ID id;
 
-  PolymorphicReference(this.id, this.type);
+  PolymorphicReference(this.type, this.id);
 
   @override
   String toString() {
-    return 'PolymorphicReference{id: $id, type: $type}';
+    return 'PolymorphicReference{type: $type, id: $id}';
   }
 
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
-    if (other is! PolymorphicReference<A, B>) return false;
+    if (other is! PolymorphicReference<T, ID>) return false;
 
     return other.id == id && other.type == type;
   }
