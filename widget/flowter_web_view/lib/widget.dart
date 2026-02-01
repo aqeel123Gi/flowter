452b3ccb3c1b6ddbf06dc19ac16e5afe34b68cfc
aqeel123gi0ta <a.aljutail@tahdir.sa>
2026-02-01 12:28:44 +0300

    Change packages versions to solve conflicts

--- widget/flowter_web_view/lib/widget.dart
+++ widget/flowter_web_view/lib/widget.dart
@@ -1,10 +1,8 @@
 part of 'flowter_web_view.dart';
 
-class FlowterWebView extends StatefulWidget{
-
-
-  static bool get supportedPlatform{
-    switch(Platform.operatingSystem){
+class FlowterWebView extends StatefulWidget {
+  static bool get supportedPlatform {
+    switch (Platform.operatingSystem) {
       case "android":
       case "ios":
       case "macos":
@@ -15,17 +13,14 @@ class FlowterWebView extends StatefulWidget{
     }
   }
 
-
   const FlowterWebView({
     super.key,
     this.headers = const {},
     required this.startUrl,
     this.onUrlChanged,
-
-
   });
 
-  final Map<String,String> headers;
+  final Map<String, String> headers;
   final String startUrl;
   final void Function(String url)? onUrlChanged;
 
@@ -34,7 +29,6 @@ class FlowterWebView extends StatefulWidget{
 }
 
 class _FlowterWebViewState extends State<FlowterWebView> {
-
   final CrossWebViewController _controller = CrossWebViewController();
 
   @override
@@ -43,11 +37,10 @@ class _FlowterWebViewState extends State<FlowterWebView> {
         widget: widget,
         controller: _controller,
         builder: (context, c) {
-
-          switch(Platform.operatingSystem){
+          switch (Platform.operatingSystem) {
             case "android":
               return WebViewWidget(
-                  controller: _controller.webViewController,
+                controller: _controller.webViewController,
               );
             case "ios":
             case "macos":
@@ -56,32 +49,30 @@ class _FlowterWebViewState extends State<FlowterWebView> {
                 color: Colors.white,
                 child: InAppWebView(
                   initialUrlRequest: URLRequest(
-                      headers: widget.headers,
-                      url: WebUri(widget.startUrl)),
+                      headers: widget.headers, url: Uri.parse(widget.startUrl)),
                   onWebViewCreated: (controller) {
                     _controller.inAppWebViewController = controller;
                   },
                   onLoadStart: (controller, url) {
-                    if(url!=null) {
+                    if (url != null) {
                       widget.onUrlChanged?.call(url.path);
                     }
                   },
-                  onUpdateVisitedHistory: (controller, url, _){
-                    if(url!=null) {
-                      widget.onUrlChanged?.call(url.origin+url.host+url.path+(url.query.isNotEmpty?"?${url.query}":""));
+                  onUpdateVisitedHistory: (controller, url, _) {
+                    if (url != null) {
+                      widget.onUrlChanged?.call(url.origin +
+                          url.host +
+                          url.path +
+                          (url.query.isNotEmpty ? "?${url.query}" : ""));
                     }
                   },
                 ),
               );
-              // return Webview(_controller.webViewController2);
+            // return Webview(_controller.webViewController2);
             default:
-              throw Exception("Unsupported platform: ${Platform.operatingSystem}");
+              throw Exception(
+                  "Unsupported platform: ${Platform.operatingSystem}");
           }
-
-
-        }
-    );
+        });
   }
-
-
 }
