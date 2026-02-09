30b29c70ae21a4cf794d1f84bc12562a9a7ea8a5
aqeel123gi0ta <a.aljutail@tahdir.sa>
2026-02-09 11:29:01 +0300

    update

--- flowter_core/lib/functions/audio.dart
+++ flowter_core/lib/functions/audio.dart
@@ -1,39 +1,18 @@
-import 'package:just_audio/just_audio.dart' as p1;
+import 'package:just_audio/just_audio.dart';
+
+AudioPlayer _audioPlayer = AudioPlayer();
 
 Future<void> playAudioFromAssets(String path, {int? seconds}) async {
-  //TODO: TEST ON WINDOWS
 
-  // if(!Platform.isWindows){
-  p1.AudioPlayer audioPlayer = p1.AudioPlayer();
-  Duration? duration = await audioPlayer.setAsset('assets/$path');
+  Duration? duration = await _audioPlayer.setAsset('assets/$path');
   if (seconds != null) {
-    audioPlayer.setLoopMode(p1.LoopMode.one);
-    audioPlayer.play();
+    await _audioPlayer.setLoopMode(LoopMode.one);
+    await _audioPlayer.play();
     await Future.delayed(Duration(seconds: seconds));
-    await audioPlayer.stop();
+    await _audioPlayer.stop();
   } else {
-    audioPlayer.play();
+    await _audioPlayer.play();
     await Future.delayed(duration! + const Duration(milliseconds: 200));
-    await audioPlayer.stop();
+    await _audioPlayer.stop();
   }
-  await audioPlayer.dispose();
-  // }
-  // else{
-
-  //   p2.AudioPlayer audioPlayer = p2.AudioPlayer();
-  //   await audioPlayer.setSource(p2.AssetSource(path));
-  //   Duration? duration = await audioPlayer.getDuration();
-
-  //   if(seconds!=null){
-  //     audioPlayer.play(p2.AssetSource(path));
-  //     await Future.delayed(Duration(seconds: seconds));
-  //     await audioPlayer.stop();
-  //   }else {
-  //     audioPlayer.play(p2.AssetSource(path));
-  //     await Future.delayed(duration! + const Duration(milliseconds: 200));
-  //     await audioPlayer.stop();
-  //   }
-  //   await audioPlayer.dispose();
-
-  // }
 }
